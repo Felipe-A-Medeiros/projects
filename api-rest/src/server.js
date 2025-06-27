05b2f723a864c1f0dc2f7f7f78d28f62a44686a9
Felipe-A-Medeiros <felipeazevedomm@gmail.com>
2025-06-26 22:47:31 -0300

    chore: restore .gitignore and add package.json and package-lock.json for project dependencies

--- api-rest/src/server.js
+++ api-rest/src/server.js
@@ -2,7 +2,7 @@ import app from './app.js'; // Importa o aplicativo Express
 
 import conexao from './app/database/conexao.js'; // Importa a conexão com o banco de dados
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000
 
 // fazera conexão com o banco de dados
 conexao.connect((erro) => {
