d8fe7db4ad84d09c0198a7011659bb873f263216
Felipe-A-Medeiros <felipeazevedomm@gmail.com>
2025-06-27 16:27:16 -0300

    integrando back e front

--- api-rest/src/app.js
+++ api-rest/src/app.js
@@ -1,7 +1,9 @@
 import express from 'express' 
 import router from './routes.js'
+import cors from 'cors' // Importa o middleware CORS para permitir requisições de diferentes origens
 const app = express() 
 
+app.use(cors()) // Usar o cors para permitir requisições de diferentes origens
 
 //indicar que o express vai receber dados no formato JSON
 app.use(express.json()) 
